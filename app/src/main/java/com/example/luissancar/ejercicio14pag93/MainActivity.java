446d8dc7b0154d4f008747001bdb05ecd7e089ef
luissancar <luissancar@yahoo.es>
2016-09-01 17:45:55 +0200

    Final

--- app/src/main/java/com/example/luissancar/ejercicio14pag93/MainActivity.java
+++ app/src/main/java/com/example/luissancar/ejercicio14pag93/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         et1=(EditText)findViewById(R.id.editText);
+        String[] ficheros=fileList();
+        if (existe(ficheros,"notas.txt"))
+            try {
+                InputStreamReader fichero=new InputStreamReader(openFileInput("notas.txt"));
+                BufferedReader br=new BufferedReader(fichero);
+                String linea=br.readLine();
+                String texto="";
+                while(linea!=null) {
+                    texto=texto+linea+"\n";
+                    linea=br.readLine();
+                }
+                br.close();
+                et1.setText(texto.toString());
 
+
+            }
+            catch (IOException e){
+
+                Toast toast = Toast.makeText(this, "Error: "+e.getMessage().toString(), Toast.LENGTH_LONG);
+                toast.show();
+                finish();
+
+            }
+
+
+    }
+
+    private boolean existe(String[] ficheros,String cadena ){
+        for (int i=0; i<ficheros.length; i++){
+            if (cadena.equals(ficheros[i]))
+                return true;
+        }
+        return false;
     }
 
     public void grabar(View view){
